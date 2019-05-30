a4441ac61edbfdedf8229a7afef0f1374163cf0f
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-05-30 17:53:05 +0430

    urls has been edited for testing

---
+++ Backlog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from Backlog import views
+
+urlpatterns = [
+    url(r '^',views.BacklogList.as_view()),
+]
+
--- ttsn/urls.py
+++ ttsn/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from Backlog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/accounts/',include("accounts.urls")),
     url(r'^api/Task/',include("Task.urls")),
     url(r'^swagger/',include("swagger.urls")),
-    url(r'^api/Backlog/',include("Backlog.urls")),
+    url(r'^api/Backlog/',views.BacklogList.as_view()),
 ]
