3b42184a5c7061c71a551d69bc38b4a5fb099eda
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-05-30 17:29:30 +0430

    Backlog serializers and models are getting shape

--- Backlog/serializers.py
+++ Backlog/serializers.py
@@ -3,4 +3,14 @@ from .models import Backlog
 class BacklogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Backlog
-        fields = ('name','priority','defenition_done','description')
+        # fields = ('name','priority','defenition_done','description')
+        fields = '__all__'
+    def create(self,validated_data):
+        id = validated_data['id']
+        name = validated_data['name']
+        priority = validated_data['priority']
+        defenition_done = validated_data['defenition_done']
+        description = validated_data['description']
+        backlog_obj = Backlog(id,name,property,defenition_done,description)
+        backlog_obj.save()
+        return validated_data
--- Backlog/views.py
+++ Backlog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from Backlog.serializers import BacklogSerializer
+from Backlog.models import Backlog
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class BacklogList(APIView):
+    
+    def get(self,request,format = None):
+        Backlogs = Backlog.objects.all()
+        serializer = BacklogSerializer(Backlogs,many = True)
+        return(serializer.data)
+    def post(self, request, format=None):
+        serializer = BacklogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
