72ce9f4964a6222fec7a40318ae115743535a63c
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-06-01 14:04:58 +0430

    FMA

--- Backlog/serializers.py
+++ Backlog/serializers.py
@@ -5,12 +5,12 @@ class BacklogSerializer(serializers.ModelSerializer):
         model = Backlog
         # fields = ('name','priority','defenition_done','description')
         fields = '__all__'
-    def create(self,validated_data):
-        id = validated_data['id']
-        name = validated_data['name']
-        priority = validated_data['priority']
-        defenition_done = validated_data['defenition_done']
-        description = validated_data['description']
-        backlog_obj = Backlog(id,name,property,defenition_done,description)
-        backlog_obj.save()
-        return validated_data
+    # def create(self,validated_data):
+    #     # idd = validated_data['id']
+    #     name = validated_data['name']
+    #     priority = validated_data['priority']
+    #     defenition_done = validated_data['defenition_done']
+    #     description = validated_data['description']
+    #     backlog_obj = Backlog(name,property,defenition_done,description)
+    #     backlog_obj.save()
+    #     return validated_data
