e4226c0408f3a62d174ed6243a564e67cf0e5cbc
code <code@localhost.localdomain>
2019-06-02 22:36:37 -0400

    swagger updated

--- swagger/views.py
+++ swagger/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
 
-# Create your views here.
+schema_view = get_swagger_view(title='Pastebin API')
+
+urlpatterns = [
+    url(r'^$', schema_view)
+]
