51485758011b8b170499e3e3d384367cf8758157
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-06-11 02:50:18 +0430

    Edit backlog field name to definition_done

--- Backlog/models.py
+++ Backlog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Backlog(models.Model):
     name = models.TextField(max_length=50)
     priority = models.IntegerField(unique=True)
-    defenition_done = models.TextField(max_length=150)
+    definition_done = models.TextField(max_length=150)
     description = models.TextField(max_length=150)
     create_date = models.DateField(auto_now=True)
     def __str__(self):
