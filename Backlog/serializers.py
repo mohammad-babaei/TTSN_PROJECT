98b05a8c25a850a4ca90b1181572f5fb6bd24187
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-06-14 01:44:12 +0430

    Update create method in backlogserializer

--- Backlog/serializers.py
+++ Backlog/serializers.py
@@ -1,37 +1,35 @@
 from rest_framework import serializers
 from .models import Backlog
+# class BacklogListSerializer(serializers.ListSerializer):
+#     pass
+
 class BacklogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Backlog
         # fields = ('name','priority','defenition_done','description')
         fields = '__all__'
-        read_only_fields = ('priority',)
+        # read_only_fields = ('priority',)
         # exclude = ('priority',)
+        # list_serializer_class = CustomListSerializer
     def create(self,validated_data):
-        Backlog_list = Backlog.objects.all().order_by("-id")
-        if(Backlog_list):
-            max_id = Backlog_list[0].id
-            print(max_id)
-            backlog = Backlog(name = validated_data["name"],definition_done = validated_data["definition_done"],description = validated_data["description"],priority = max_id+1)
-            backlog.save()
-        else:
-            backlog = Backlog(name = validated_data["name"],definition_done = validated_data["definition_done"],description = validated_data["description"],priority = 0)
-            backlog.save()
+        Backlog_list = Backlog.objects.all()
+        # if(not Backlog_list):
+        #     validated_data["priority"] = 1
+        # else:
+        max_id = len(Backlog_list)
+        if(validated_data["priority"]==None):
+            validated_data["priority"]=max_id+1
+        elif (validated_data["priority"]>max_id):
+            validated_data["priority"]=max_id+1 
+        elif (validated_data["priority"]<max_id):
+            # Backlog_list = Backlog.objects.all().order_by("priority")
+            for i in range(max_id,validated_data["priority"]-1,-1):
+                Backlog.objects.filter(priority=i).update(priority=i+1)
+
+        backlog = Backlog(name = validated_data["name"],definition_done = validated_data["definition_done"],description = validated_data["description"],priority = validated_data["priority"])
+        backlog.save()
         return validated_data
-# class BacklogTaskSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Backlog
-#         # fields = ('name','priority','defenition_done','description')
-#         fields = ('id')
+
 
 
             
-    # def create(self,validated_data):
-    #     # idd = validated_data['id']
-    #     name = validated_data['name']
-    #     priority = validated_data['priority']
-    #     defenition_done = validated_data['defenition_done']
-    #     description = validated_data['description']
-    #     backlog_obj = Backlog(name,property,defenition_done,description)
-    #     backlog_obj.save()
-    #     return validated_data
