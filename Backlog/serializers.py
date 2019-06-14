d4f56dbd2cb849e09746f61a5640500549928d9b
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-06-14 14:56:06 +0430

    Override destroy method in backlogserializer

--- Backlog/serializers.py
+++ Backlog/serializers.py
@@ -1,35 +1,39 @@
 from rest_framework import serializers
 from .models import Backlog
-# class BacklogListSerializer(serializers.ListSerializer):
-#     pass
 
 class BacklogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Backlog
-        # fields = ('name','priority','defenition_done','description')
         fields = '__all__'
-        # read_only_fields = ('priority',)
-        # exclude = ('priority',)
-        # list_serializer_class = CustomListSerializer
+    # def destroy(self, request, *args, **kwargs):
+    #     try:
+    #         instance = self.get_object()
+    #         self.perform_destroy(instance)
+    #     except Http404:
+    #         pass
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    def perform_destroy(self, instance):
+        instance.delete()
     def create(self,validated_data):
         Backlog_list = Backlog.objects.all()
-        # if(not Backlog_list):
-        #     validated_data["priority"] = 1
-        # else:
         max_id = len(Backlog_list)
         if(validated_data["priority"]==None):
             validated_data["priority"]=max_id+1
         elif (validated_data["priority"]>max_id):
             validated_data["priority"]=max_id+1 
         elif (validated_data["priority"]<max_id):
-            # Backlog_list = Backlog.objects.all().order_by("priority")
             for i in range(max_id,validated_data["priority"]-1,-1):
                 Backlog.objects.filter(priority=i).update(priority=i+1)
-
         backlog = Backlog(name = validated_data["name"],definition_done = validated_data["definition_done"],description = validated_data["description"],priority = validated_data["priority"])
         backlog.save()
         return validated_data
 
+        # pr = request.user.priority
+        # Backlog_list = Backlog.objects.all()
+        # max_id = len(Backlog_list)
+        # for i in range(pr+1,max_id+1):
+        #     Backlog.objects.filter(priority=i).update(priority=i-1)
+        # return super(BacklogSerializer, self).destroy(request, *args, **kwargs)            
 
 
             
