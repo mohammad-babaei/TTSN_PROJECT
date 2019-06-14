1a6ed2fa7816f3fad633d46dfae71b95220c0db8
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-06-14 15:14:17 +0430

    Add id and created-date to backlog post response body

--- Backlog/serializers.py
+++ Backlog/serializers.py
@@ -17,7 +17,7 @@ class BacklogSerializer(serializers.ModelSerializer):
     def create(self,validated_data):
         Backlog_list = Backlog.objects.all()
         max_id = len(Backlog_list)
-        if(validated_data["priority"]==None):
+        if(validated_data["priority"]==None or validated_data["priority"]<=0):
             validated_data["priority"]=max_id+1
         elif (validated_data["priority"]>max_id):
             validated_data["priority"]=max_id+1 
@@ -26,6 +26,8 @@ class BacklogSerializer(serializers.ModelSerializer):
                 Backlog.objects.filter(priority=i).update(priority=i+1)
         backlog = Backlog(name = validated_data["name"],definition_done = validated_data["definition_done"],description = validated_data["description"],priority = validated_data["priority"])
         backlog.save()
+        validated_data['id'] = backlog.id
+        validated_data['create_date'] = backlog.create_date
         return validated_data
 
         # pr = request.user.priority
