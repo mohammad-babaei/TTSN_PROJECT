5cfc76831b26c30a34771337c90760b3e48aa468
Mahdi Pazoki <mahdipazooki21@gmail.com>
2019-06-15 13:19:16 +0430

    TaskRelatedField added

--- Task/serializers.py
+++ Task/serializers.py
@@ -4,12 +4,29 @@ from rest_framework import serializers
 from .models import Task
 
 
+
+
+class TaskPickerRelatedField(serializers.RelatedField):
+
+    def to_representation(self,value):
+        data = (value.username,value.email,value.profile_picture.url)
+        # data = {
+        #     "username":value.username,
+        #     "email":value.email,
+        #     "username":value.profile_picture,
+        #     }
+        return data
+
+
+
+
 class TaskSerializer(serializers.ModelSerializer):
 
-    picker = HyperlinkedRelatedField(
-        read_only=True,
-        view_name='users-detail'
-    )
+    # picker = HyperlinkedRelatedField(
+    #     read_only=True,
+    #     view_name='users-detail'
+    # )
+    picker = TaskPickerRelatedField(read_only = True)
     class Meta:
         model = Task
         fields = (
