0f3036ddc2dfaeafc5c0abde08c5a0f33bcf5913
code <code@localhost.localdomain>
2019-07-04 00:56:45 -0400

    migrations updated

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,6 @@ class users (AbstractUser):
     password = models.CharField(max_length = 100)
     email = models.EmailField(max_length = 100)
     joined_date = models.DateField(auto_now=True)
-    projects = models.TextField(null=True)
     bio = models.TextField(null=True)
     # profile_pic = models.FileField(upload_to="profile_pics/%Y/%m/%d/", max_length=100,blank=True,null=False)
     profile_picture = models.ImageField(default = "profile_pics/profile-default-male.png", upload_to="profile_pics/%Y/%m/%d/", height_field=None, width_field=None, max_length=None,null=False)
