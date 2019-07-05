3b6043807ac00dc2021437825410f34033ff1d75
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-07-05 19:40:34 +0430

    Edit priority_list_view's logic

--- Backlog/views.py
+++ Backlog/views.py
@@ -76,9 +76,10 @@ class BacklogModelViewSet(viewsets.ModelViewSet):
             instances= Backlog.objects.filter(priority=i+1)
             if(instances):
                 idd = instances[0].id
-                id_newpriority.append((idd,new_priorities[i]))
+                id_newpriority.append((idd,new_priorities.index(instances[0].priority)+1))
         print(id_newpriority)
         for id_prio in id_newpriority:
             Backlog.objects.filter(id=id_prio[0]).update(priority=id_prio[1])
+        objects = Backlog.objects.order_by("priority")
         return Response("HTTP_200_OK" ,status=status.HTTP_200_OK)
 
